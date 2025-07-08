52f13b0970d33d0e52088bd7767ed3090b042f0d
soham1105-tech <raysoham2005@gmail.com>
2025-07-08 18:48:21 +0000

    operation changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,3 +20,12 @@ let someNumber = 4356798
 let stringSomeNumber = String(someNumber)
 console.log(stringSomeNumber)
 console.log(typeof stringSomeNumber)
+
+//**************************** Operations ***************************
+
+console.log("1"+7);
+console.log(1+"7");
+console.log("1" + 2 + 2); //122 if string in first then every thing will be treated as string
+console.log(1 + 2 + "2"); //32 if number is first then every thing will be treated as number except ""
+console.log(+true) //boolean changes to number 1 
+console.log(+"") //null changes to number 0
