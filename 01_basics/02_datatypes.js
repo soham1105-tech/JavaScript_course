49d85ae8e539b159b95170f00e486ba04826b537
soham1105-tech <raysoham2005@gmail.com>
2025-07-08 14:48:25 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS codes as newer version...
+
+// alert( 3 + 3 ) //we are using node.js, not browser
+
+console.log( 3 + 3 )
+console.log("Soham Ray")
+
+var name = "Soham";
+var age = 20;
+var isLoggedIn = true;
+var vary = null;
+console.log([ age, isLoggedIn, vary, ]);
+
+//object
+console.log(typeof vary); //object (null is a special case in JS, it is an object type but represents an empty value)
+
+// PRIMITIVE DATATYPES
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value(khali) (empty value)
+// undefined => value is not assigned yet
+// symbol => unique value (ES6)
