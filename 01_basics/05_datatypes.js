58cbcca0a3029ddd0bd8fb993873292e5a8f3e1d
soham1105-tech <raysoham2005@gmail.com>
2025-07-09 11:20:02 +0000

    datatype memory localisation

--- 01_basics/05_datatypes.js
+++ 01_basics/05_datatypes.js
@@ -35,3 +35,27 @@ console.log(typeof id)
 //      more complex entities.
 //    c)Objects are defined using curly braces {} and can contain multiple key-value pairs,   
 //      while functions can be treated as first-class objects.
+
+// +++++++++++++++++++++++ MEMORY +++++++++++++++++++++++
+// Stack(Primitive), Heap(Non-Primitive)
+
+//Stack Example => Change in Value of the Variable
+let actualName = "Soham Ray";
+let anotherName = actualName; // Copying value
+console.log(actualName, anotherName);
+anotherName = "Batman"; // Changing anotherName does not affect actualName
+console.log(actualName, anotherName);
+
+//Heap Example => Change in Reference of the Value
+    let userOne = {
+        email: "userOne@gmail.com",
+        upi: "userOne@ybl",
+    }
+    let userTwo = userOne; // Copying reference
+
+    console.log(userOne, userTwo);
+
+    userTwo.email = "userTwo@gmail.com"; // Changing userTwo affects userOne
+    console.log(userOne, userTwo); // Both show the updated email
+
+
