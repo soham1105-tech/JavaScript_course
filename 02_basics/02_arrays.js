955b93a18b4d6357de5018ac0df1a50a8b568a3d
soham1105-tech <raysoham2005@gmail.com>
2025-07-10 10:20:58 +0000

    array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -2,4 +2,8 @@ const marvel_heroes = ["Thor", "Spiderman", "Ironman"]
 const dc_heroes = ["Superman", "Batman", "Flash"]
 
 marvel_heroes.push(dc_heroes)
-console.log(marvel_heroes); //array inside array
+console.log(marvel_heroes); //array inside array //array is a single element here
+console.log(marvel_heroes[3]) //array is a single element here
+
+const heroes = marvel_heroes.concat(dc_heroes)
+console.log(heroes);
