74ab4eab88f5beadd4e0f7329aacabff69a74487
soham1105-tech <raysoham2005@gmail.com>
2025-07-10 07:16:12 +0000

    arrays 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,5 @@
+const marvel_heroes = ["Thor", "Spiderman", "Ironman"]
+const dc_heroes = ["Superman", "Batman", "Flash"]
+
+marvel_heroes.push(dc_heroes)
+console.log(marvel_heroes); //array inside array
